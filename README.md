b559bed630628a9faa6ed6d360af1ff393f24a65
Noah.Shmais <nawarmekhaiel@gmail.com>
2017-06-04 15:35:25 -0500

    create files

---
+++ README.md
@@ -0,0 +1,58 @@
+# Neighborhood Map
+<img src="img/map.png" alt="map" align="right" height="150"> 
+
+The main task of this project is to develop a single page application featuring a map of my neighborhood. The map include many functionality such as creating markers and highlighted locations, third-party data about those locations and various ways to browse the content.
+
+
+
+## Google API key
+<img src="img/key_icon.png" height="25">In order to use Google_Maps API we will meed a key. **Make sure you replace "MYAPIKEY" in the JavaScript API call with your generated API key in code.** please, follow the instruction bellow to get the key: 
+1. Visit [console.developers.google](https://console.developers.google.com)
+2. Sign in with your Google - i.e. yourname@gmail.com. If you don't have one, you can go to [accounts.google.com](https://myaccount.google.com/) to create one for free.
+3. Click on "Select a Project" and then "Create a Project" in the upper right side of
+<img src="img/3.PNG">
+
+4. Name your project something descriptive, such as "Maps-Project" or "Emily-likes-buffalo-wings". Select whether you'd like to receive updates (usually a good idea) and select whether you agree to the terms of service. 
+<img src="img/4.PNG">
+
+5. Click the "Create" button.
+6. Click on the "Google Maps JavaScript API" under "Google Maps APIs" 
+<img src="img/6.PNG">
+
+7. Click the "Enable" button
+8. Repeat steps 6 and 7 for the following APIs (highlighted in yellow): 
+<img src="img/8.PNG">
+
+9. Click the “Credentials” menu item on the left of the screen
+10. Click the “Create Credentials” button
+11. Select “API Key” from the options that appear.
+<img src="img/11.PNG">
+
+12. Select the “Browser Key” button
+13. Name your API Key something descriptive, such as Maps_API. It should reflect your intended use of the key. Click the “Create” button 
+<img src="img/13.PNG">
+
+14. Repeat steps 10 and 11 for “Server Key,” if you will be making server-side requests. You can access these keys at any time by visiting the “Credentials” sub-menu under the “API Manager” menu
+
+## Code Explanation
+
+
+## Geocoding
+[Geocoding](https://developers.google.com/maps/documentation/geocoding/intro) is the process of converting addresses (like "1600 Amphitheatre Parkway, Mountain View, CA") into geographic coordinates (like latitude 37.423021 and longitude -122.083739), which you can use to place markers on a map, or position the map and the other way around.
+To get Geocoding service (**Address to location**), you need to google the following URL with the required parameters.
+```
+http://maps.googleapis.com/maps/api/geocode/outputFormat?parameters
+```
+This is an example of Six Flags location in Arlington, Texas. You can click the link [here](http://maps.googleapis.com/maps/api/geocode/json?address=2201+Road+to+Six+Flags+Arlington+TX) 
+```
+http://maps.googleapis.com/maps/api/geocode/json?address=2201+Road+to+Six+Flags+Arlington+TX
+```
+then we look for location in the result: 
+```
+ "location" : {
+               "lat" : 32.75570450000001,
+               "lng" : -97.07259839999999
+            }
+```
+## License
+`Neighborhood_Map` is a released under the [MIT License](https://opensource.org/licenses/MIT)
---
+++ js/app.js
@@ -0,0 +1,278 @@
+var locations = [
+          {title: 'Water Gardens',
+           about:'Built in 1974,designed by Phillip Johnson',
+           address:'1502 Commerce St, Fort Worth, TX 76102',
+           location: {lat: 32.7477, lng: -97.3266}},
+
+          {title: 'Stockyards Station',
+           about:'140 E Exchange Ave, Fort Worth, TX 76164',
+           address:'140 E Exchange Ave, Fort Worth, TX 76164',
+           location: {lat: 32.7886, lng: -97.3462}},
+
+          {title: 'Botanic Garden',
+           about:'Home to more than 2,500 species of plants in its 23 specialty gardens',
+           address:'3220 Botanic Garden Blvd, Fort Worth, TX 76107',
+           location: {lat: 32.7402, lng: -97.3639}},
+
+          {title: 'Texas Christian University',
+           about:'Established in 1873 by Addison & Randolph Clark as the AddRan Male & Female College',
+           address:'2800 S University Dr, Fort Worth, TX 76129',
+           location: {lat: 32.7095, lng: -97.3628}},
+
+          {title: 'Fort Worth Zoo',
+           about:'The zoo now home to 5,000 native and exotic animals ',
+           address:'1989 Colonial Pkwy, Fort Worth, TX 76110',
+           location: {lat: 32.7230, lng: -97.3567}},
+
+          {title: 'Kimbell Art Museum',
+           about:'Hosts an art collection as well as traveling art exhibitions, educational programs and an extensive research library.',
+           address:'3333 Camp Bowie Blvd, Fort Worth, TX 76107',
+           location: {lat: 32.7486, lng: -97.3693}},
+
+          {title: 'Museum of Science and History',
+           about:'Provides hundreds of programs aimed at engaging children and families in learning',
+           address:'1600 Gendy St, Fort Worth, TX 76107',
+           location: {lat: 32.7442, lng: -97.3693}}
+        ];
+
+var Location = function(data){
+    this.title = ko.observable(data.title);
+    this.location = ko.observable(data.location);
+    this.about = ko.observable(data.about);
+    this.address = ko.observable(data.address);
+}
+
+var ViewModel = function(){
+    var self=this;
+    // creat a locationList array to loop all the data
+    this.locationList = ko.observableArray([]);
+    locations.forEach(function(locationItem){
+        self.locationList.push(new Location(locationItem));
+    })
+    this.CurrentLocation = ko.observable(self.locationList()[0]);
+// display a cliked landmark
+    this.setLandmark=function(index){
+        self.CurrentLocation(self.locationList()[index]);
+    };
+}
+
+ko.applyBindings(new ViewModel());
+
+
+
+// Create a global variable for map
+var map;
+// Create a new blank array for all the markers.
+var markers = [];
+// Create a styles array to use with the map.
+var styles = [
+  {
+    featureType: 'water',
+    stylers: [
+      { color: '#19a0d8' }
+    ]
+  },{
+    featureType: 'administrative',
+    elementType: 'labels.text.stroke',
+    stylers: [
+      { color: '#ffffff' },
+      { weight: 6 }
+    ]
+  },{
+    featureType: 'administrative',
+    elementType: 'labels.text.fill',
+    stylers: [
+      { color: '#e85113' }
+    ]
+  },{
+    featureType: 'road.highway',
+    elementType: 'geometry.stroke',
+    stylers: [
+      { color: '#efe9e4' },
+      { lightness: -40 }
+    ]
+  },{
+    featureType: 'transit.station',
+    stylers: [
+      { weight: 9 },
+      { hue: '#e85113' }
+    ]
+  },{
+    featureType: 'road.highway',
+    elementType: 'labels.icon',
+    stylers: [
+      { visibility: 'off' }
+    ]
+  },{
+    featureType: 'water',
+    elementType: 'labels.text.stroke',
+    stylers: [
+      { lightness: 100 }
+    ]
+  },{
+    featureType: 'water',
+    elementType: 'labels.text.fill',
+    stylers: [
+      { lightness: -100 }
+    ]
+  },{
+    featureType: 'poi',
+    elementType: 'geometry',
+    stylers: [
+      { visibility: 'on' },
+      { color: '#f0e4d3' }
+    ]
+  },{
+    featureType: 'road.highway',
+    elementType: 'geometry.fill',
+    stylers: [
+      { color: '#efe9e4' },
+      { lightness: -25 }
+    ]
+  }
+];
+
+
+
+      function initMap() {
+        //creates a new map - only center and zoom are required stayle can be commented-out for defult style.
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 32.768799, lng: -97.309341},
+          zoom: 10,
+          styles: styles
+        });
+
+
+        var largeInfowindow = new google.maps.InfoWindow();
+        // Style the marker icon (input a color code).
+        var defaultIcon = makeMarkerIcon('00FF00');
+        //"highlight marker color for when mousesover the marker (input a color code).
+        var highlightedIcon = makeMarkerIcon('0091ff');
+        // loop over thelocations array to initialize markers.
+        for (var i = 0; i < locations.length; i++) {
+          // Get the position from the locations array.
+          var position = locations[i].location;
+          var title = locations[i].title;
+          // Create a marker per location, and put into markers array.
+          var marker = new google.maps.Marker({
+            position: position,
+            title: title,
+            draggable: true,
+            animation: google.maps.Animation.DROP,
+            icon: defaultIcon,
+            id: i
+          });
+          // Push the marker to our array of markers.
+          markers.push(marker);
+          // Create an onclick event to open the large infowindow at each marker.
+          marker.addListener('click', function() {
+            populateInfoWindow(this, largeInfowindow);
+          });
+          // Two event listeners - one for mouseover, one for mouseout,
+          // to change the colors back and forth of the marker icon.
+          marker.addListener('mouseover', function() {
+            this.setIcon(highlightedIcon);
+          });
+          marker.addListener('mouseout', function() {
+            this.setIcon(defaultIcon);
+          });
+        }
+
+        document.getElementById('show-Landmarks').addEventListener('click', showLandmarks);
+        document.getElementById('hide-Landmarks').addEventListener('click', hideLandmarks);
+
+      }
+
+
+      // This function populates the infowindow when the marker is clicked. We'll only allow
+      // one infowindow which will open at the marker that is clicked, and populate based
+      // on that markers position.
+      function populateInfoWindow(marker, infowindow) {
+        // Check to make sure the infowindow is not already opened on this marker.
+        if (infowindow.marker != marker) {
+          // Clear the infowindow content to give the streetview time to load.
+          infowindow.setContent('');
+          infowindow.marker = marker;
+          // Make sure the marker property is cleared if the infowindow is closed.
+          infowindow.addListener('closeclick', function() {
+            infowindow.marker = null;
+          });
+          var streetViewService = new google.maps.StreetViewService();
+          var radius = 100;
+          // In case the status is OK, which means the pano was found, compute the
+          // position of the streetview image, then calculate the heading, then get a
+          // panorama from that and set the options
+          function getStreetView(data, status) {
+            if (status == google.maps.StreetViewStatus.OK) {
+              var nearStreetViewLocation = data.location.latLng;
+              var heading = google.maps.geometry.spherical.computeHeading(
+                nearStreetViewLocation, marker.position);
+                infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
+                var panoramaOptions = {
+                  position: nearStreetViewLocation,
+                  pov: {
+                    heading: heading,
+                    pitch: 30
+                  }
+                };
+              var panorama = new google.maps.StreetViewPanorama(
+                document.getElementById('pano'), panoramaOptions);
+            } else {
+              infowindow.setContent('<div>' + marker.title + '</div>' +
+                '<div>No Street View Found</div>');
+            }
+          }
+          // Use streetview service to get the closest streetview image within
+          // 100 meters radius of the markers position
+          streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+          // Open the infowindow on the correct marker.
+          infowindow.open(map, marker);
+        }
+      }
+
+      // This function will loop through the markers array and display them all.
+      function showLandmarks() {
+        var bounds = new google.maps.LatLngBounds();
+        // Extend the boundaries of the map for each marker and display the marker
+        for (var i = 0; i < markers.length; i++) {
+          markers[i].setMap(map);
+          bounds.extend(markers[i].position);
+        }
+        map.fitBounds(bounds);
+      }
+
+      // This function will loop through the markers and hide them all.
+      function hideLandmarks() {
+        for (var i = 0; i < markers.length; i++) {
+          markers[i].setMap(null);
+        }
+      }
+
+      // This function takes in a COLOR, and then creates a new marker
+      // icon of that color. The icon will be 21 px wide by 34 high, have an origin
+      // of 0, 0 and be anchored at 10, 34).
+      function makeMarkerIcon(markerColor) {
+        var markerImage = new google.maps.MarkerImage(
+          'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+          '|40|_|%E2%80%A2',
+          new google.maps.Size(21, 34),
+          new google.maps.Point(0, 0),
+          new google.maps.Point(10, 34),
+          new google.maps.Size(21,34));
+        return markerImage;
+      }
+
+
+        //bounce the marker when click on its title
+   function Bounce(index) {
+    var marker = markers[index];
+    if (marker.getAnimation() !== null) {
+      marker.setAnimation(null);
+    } else {
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+      //toggleBounce the marker for 5 seconds
+      setTimeout(function(){
+      marker.setAnimation(null);
+      },5000);
+    }
+  }
