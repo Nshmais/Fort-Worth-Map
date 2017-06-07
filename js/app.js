c45bc4b82a900f73051407c1ea623e04caa46d28
Noah.Shmais <nawarmekhaiel@gmail.com>
2017-06-07 01:28:25 -0500

    update
    separate files and add markder movements

--- js/app.js
+++ js/app.js
@@ -1,67 +1,24 @@
-var locations = [
-          {title: 'Water Gardens',
-           about:'Built in 1974,designed by Phillip Johnson',
-           address:'1502 Commerce St, Fort Worth, TX 76102',
-           website:'www.fortworth.com',
-           location: {lat: 32.7477, lng: -97.3266}},
-
-          {title: 'Stockyards Station',
-           about:'140 E Exchange Ave, Fort Worth, TX 76164',
-           address:'140 E Exchange Ave, Fort Worth, TX 76164',
-           website:'www.fortworthstockyards.org',
-           location: {lat: 32.7886, lng: -97.3462}},
-
-          {title: 'Botanic Garden',
-           about:'Home to more than 2,500 species of plants in its 23 specialty gardens',
-           address:'3220 Botanic Garden Blvd, Fort Worth, TX 76107',
-           website:'www.fwbg.org',
-           location: {lat: 32.7402, lng: -97.3639}},
-
-          {title: 'Texas Christian University',
-           about:'Established in 1873 by Addison & Randolph Clark as the AddRan Male & Female College',
-           address:'2800 S University Dr, Fort Worth, TX 76129',
-           website:'www.tcu.edu/',
-           location: {lat: 32.7095, lng: -97.3628}},
-
-          {title: 'Fort Worth Zoo',
-           about:'The zoo now home to 5,000 native and exotic animals ',
-           address:'1989 Colonial Pkwy, Fort Worth, TX 76110',
-           website:'www.fortworthzoo.org',
-           location: {lat: 32.7230, lng: -97.3567}},
-
-          {title: 'Kimbell Art Museum',
-           about:'Hosts an art collection as well as traveling art exhibitions, educational programs and an extensive research library.',
-           address:'3333 Camp Bowie Blvd, Fort Worth, TX 76107',
-           website:'www.kimbellart.org',
-           location: {lat: 32.7486, lng: -97.3693}},
-
-          {title: 'Museum of Science and History',
-           about:'Provides hundreds of programs aimed at engaging children and families in learning',
-           address:'1600 Gendy St, Fort Worth, TX 76107',
-           website:'www.fwmuseum.org',
-           location: {lat: 32.7442, lng: -97.3693}}
-        ];
-
+// create an empty Location function
 var Location = function(data){
-    this.title = ko.observable(data.title);
-    this.location = ko.observable(data.location);
-    this.about = ko.observable(data.about);
-    this.address = ko.observable(data.address);
-}
+    this.title = data.title;
+    this.location = data.location;
+    this.about = data.about;
+    this.address = data.address;
+};
 
 var ViewModel = function(){
     var self=this;
-    // creat a locationList array to loop all the data
+    // creat a locationList array to loop all the data and map Location function
     this.locationList = ko.observableArray([]);
     locations.forEach(function(locationItem){
         self.locationList.push(new Location(locationItem));
-    })
+    });
     this.CurrentLocation = ko.observable(self.locationList()[0]);
 // display a cliked landmark
     this.setLandmark=function(index){
         self.CurrentLocation(self.locationList()[index]);
     };
-}
+};
 
 ko.applyBindings(new ViewModel());
 
@@ -72,74 +29,6 @@ var map;
 // Create a new blank array for all the markers.
 var markers = [];
 // Create a styles array to use with the map.
-var styles = [
-  {
-    featureType: 'water',
-    stylers: [
-      { color: '#19a0d8' }
-    ]
-  },{
-    featureType: 'administrative',
-    elementType: 'labels.text.stroke',
-    stylers: [
-      { color: '#ffffff' },
-      { weight: 6 }
-    ]
-  },{
-    featureType: 'administrative',
-    elementType: 'labels.text.fill',
-    stylers: [
-      { color: '#e85113' }
-    ]
-  },{
-    featureType: 'road.highway',
-    elementType: 'geometry.stroke',
-    stylers: [
-      { color: '#efe9e4' },
-      { lightness: -40 }
-    ]
-  },{
-    featureType: 'transit.station',
-    stylers: [
-      { weight: 9 },
-      { hue: '#e85113' }
-    ]
-  },{
-    featureType: 'road.highway',
-    elementType: 'labels.icon',
-    stylers: [
-      { visibility: 'off' }
-    ]
-  },{
-    featureType: 'water',
-    elementType: 'labels.text.stroke',
-    stylers: [
-      { lightness: 100 }
-    ]
-  },{
-    featureType: 'water',
-    elementType: 'labels.text.fill',
-    stylers: [
-      { lightness: -100 }
-    ]
-  },{
-    featureType: 'poi',
-    elementType: 'geometry',
-    stylers: [
-      { visibility: 'on' },
-      { color: '#f0e4d3' }
-    ]
-  },{
-    featureType: 'road.highway',
-    elementType: 'geometry.fill',
-    stylers: [
-      { color: '#efe9e4' },
-      { lightness: -25 }
-    ]
-  }
-];
-
-
 
 function initMap() {
     //creates a new map - only center and zoom are required stayle can be commented-out for defult style.
@@ -171,6 +60,8 @@ function initMap() {
         website: website,
         animation: google.maps.Animation.DROP,
         icon: defaultIcon,
+        // this will display all markers by default as when load the page
+        map:map,
         id: i
       });
       // Push the marker to our array of markers.
@@ -199,6 +90,14 @@ function initMap() {
 function populateInfoWindow(marker, infowindow) {
     // Check to make sure the infowindow is not already opened on this marker.
     if (infowindow.marker != marker) {
+        //when click on the marker it will start bouncing
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+        //Bounce the marker 5 bounces
+        setTimeout(function(){
+            marker.setAnimation(null);
+            // multiple the desired number of bounces by 700ms (it take the marker 700ms for each bounce)
+        },3*700);
+
       // Clear the infowindow content to give the streetview time to load.
       infowindow.setContent('');
       infowindow.marker = marker;
@@ -283,10 +182,11 @@ function Bounce(index) {
         marker.setAnimation(null);
     } else {
         marker.setAnimation(google.maps.Animation.BOUNCE);
-        //toggleBounce the marker for 5 seconds
+        //Bounce the marker 5 bounces
         setTimeout(function(){
-        marker.setAnimation(null);
-        },5000);
+            marker.setAnimation(null);
+            // multiple the desired number of bounces by 700ms (it take the marker 700ms for each bounce)
+        },5*700);
         }
 }
 
---
+++ js/data.js
@@ -0,0 +1,43 @@
+var locations = [
+          {title: 'Water Gardens',
+           about:'Built in 1974,designed by Phillip Johnson',
+           address:'1502 Commerce St, Fort Worth, TX 76102',
+           website:'www.fortworth.com',
+           location: {lat: 32.7477, lng: -97.3266}},
+
+          {title: 'Stockyards Station',
+           about:'140 E Exchange Ave, Fort Worth, TX 76164',
+           address:'140 E Exchange Ave, Fort Worth, TX 76164',
+           website:'stockyardsstation.com',
+           location: {lat: 32.7886, lng: -97.3462}},
+
+          {title: 'Botanic Garden',
+           about:'Home to more than 2,500 species of plants in its 23 specialty gardens',
+           address:'3220 Botanic Garden Blvd, Fort Worth, TX 76107',
+           website:'www.fwbg.org',
+           location: {lat: 32.7402, lng: -97.3639}},
+
+          {title: 'Texas Christian University',
+           about:'Established in 1873 by Addison & Randolph Clark as the AddRan Male & Female College',
+           address:'2800 S University Dr, Fort Worth, TX 76129',
+           website:'www.tcu.edu',
+           location: {lat: 32.7095, lng: -97.3628}},
+
+          {title: 'Fort Worth Zoo',
+           about:'The zoo now home to 5,000 native and exotic animals ',
+           address:'1989 Colonial Pkwy, Fort Worth, TX 76110',
+           website:'www.fortworthzoo.org',
+           location: {lat: 32.7230, lng: -97.3567}},
+
+          {title: 'Kimbell Art Museum',
+           about:'Hosts an art collection as well as traveling art exhibitions, educational programs and an extensive research library.',
+           address:'3333 Camp Bowie Blvd, Fort Worth, TX 76107',
+           website:'www.kimbellart.org',
+           location: {lat: 32.7486, lng: -97.3693}},
+
+          {title: 'Museum of Science and History',
+           about:'Provides hundreds of programs aimed at engaging children and families in learning',
+           address:'1600 Gendy St, Fort Worth, TX 76107',
+           website:'www.fwmuseum.org',
+           location: {lat: 32.7442, lng: -97.3693}}
+        ];
