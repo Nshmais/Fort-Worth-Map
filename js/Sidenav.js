4065640b59e54c39e04fee6d0e50c653f2072ec0
Noah.Shmais <nawarmekhaiel@gmail.com>
2017-06-09 13:37:43 -0500

    fix
    fix the sidenav style

--- js/Sidenav.js
+++ js/Sidenav.js
@@ -1,7 +1,7 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.width = "300px";
 }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.width = "0px";
 }
